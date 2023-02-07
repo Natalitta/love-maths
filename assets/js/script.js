c80c5472348c49945d1e879633dd9e5fde44b7f6
Natalitta <nataliialebedieva@gmail.com>
2023-02-07 21:57:44 +0000

    Add function structure to the addition question

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,23 @@ document.addEventListener("DOMContentLoaded", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
-                let gameType = this.getAttribute("data-type"); {
-                    alert(`You clicked ${gameType}`);}
+                let gameType = this.getAttribute("data-type"); 
+                    runGame(gameType);
             }
         });
     }
-
+    runGame("addition");
 });
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random()*25) +1;
     let num2 = Math.floor(Math.random()*25) +1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 function checkAnswer() {
 
@@ -29,8 +35,10 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 function displaySubstractQuestion() {
 
